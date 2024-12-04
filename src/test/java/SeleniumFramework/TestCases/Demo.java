d7c30c4c4311ee5181d56b5c1c8ad2628114eca3
VenkateshRegadapa <venkatesh.regadapa@gmail.com>
2024-12-04 14:33:25 +0530

    Second commit

---
+++ src/test/java/SeleniumFramework/TestCases/Demo.java
@@ -0,0 +1,11 @@
+package SeleniumFramework.TestCases;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+    @Test
+    public void test()
+    {
+        System.out.println("Automation Test");
+    }
+}
