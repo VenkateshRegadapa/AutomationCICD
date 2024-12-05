5ee9e725ff4371ef772ebf6f88c5accdca8791dc
VenkateshRegadapa <venkatesh.regadapa@gmail.com>
2024-12-05 15:53:42 +0530

    Final commit

--- src/test/java/SeleniumFramework/TestCases/Demo.java
+++ src/test/java/SeleniumFramework/TestCases/Demo.java
@@ -6,10 +6,11 @@ public class Demo {
     @Test
     public void test1(){
         System.out.println("Automation Test1");
+        System.out.println("Automation Test2");
     }
     @Test
     public void test2(){
-        System.out.println("Automation Test2");
         System.out.println("Automation Test3");
+        System.out.println("Automation Test4");
     }
 }
