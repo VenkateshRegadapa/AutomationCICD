d62b1dbb3d96bbe5587af976a2a60f7f82ad66dc
VenkateshRegadapa <venkatesh.regadapa@gmail.com>
2024-12-05 15:52:45 +0530

    Final commit

--- src/test/java/SeleniumFramework/TestCases/Demo.java
+++ src/test/java/SeleniumFramework/TestCases/Demo.java
@@ -4,9 +4,11 @@ import org.testng.annotations.Test;
 
 public class Demo {
     @Test
-    public void test()
-    {
+    public void test1(){
         System.out.println("Automation Test1");
+    }
+    @Test
+    public void test2(){
         System.out.println("Automation Test2");
         System.out.println("Automation Test3");
     }
